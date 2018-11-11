72f34f861825a892e8010e32d4a49380b31a1d5f
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-11 22:57:05 +0100

    Reformatiran kod

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,16 +1,13 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.event.ActionEvent;
-
 
 public class Controller {
 
     private SimpleStringProperty ulaz;
 
     public Controller() {
-         ulaz = new SimpleStringProperty("0");
+        ulaz = new SimpleStringProperty("0");
     }
 
     public SimpleStringProperty ulazProperty() {
