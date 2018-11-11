7e4a6e739701e673fafbd54af6ceeb4e5aff99a4
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-12 00:42:49 +0100

    Kreirana akcija  za tipku 1

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.rpr.tutorijal05;
 
 import javafx.beans.property.SimpleStringProperty;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
 
     private SimpleStringProperty ulaz;
@@ -18,4 +20,11 @@ public class Controller {
         return ulaz.get();
     }
 
+    public void jedan(ActionEvent actionEvent) {
+        if(ulaz.get().equals("0")) {
+            ulaz.set("1");
+        } else {
+            ulaz.set(ulaz.get() + "1");
+        }
+    }
 }
