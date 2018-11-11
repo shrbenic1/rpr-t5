8c0f2d3644f0807cf2b674bf495a166641408458
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-11 22:24:12 +0100

    Omogućen property binding

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,24 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+
+
 public class Controller {
+
+    private SimpleStringProperty ulaz;
+
+    public Controller() {
+         ulaz = new SimpleStringProperty("0");
+    }
+
+    public SimpleStringProperty ulazProperty() {
+        return ulaz;
+    }
+
+    public String getUlaz() {
+        return ulaz.get();
+    }
 
 }
