2108e9b9074a4f05fc307037c5ee2b780c192230
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-12 09:34:25 +0100

    Reakcija na brojeve sažeta samo u jednu metodu

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.rpr.tutorijal05;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class Controller {
 
@@ -26,133 +27,16 @@ public class Controller {
         return ulaz.get();
     }
 
-    public void jedan(ActionEvent actionEvent) {
+    public void broj(ActionEvent actionEvent) {
         if (ulaz.get().equals("0") && operacija.equals("")) {
-            ulaz.set("1");
+            ulaz.set(((Button)actionEvent.getSource()).getText());
         } else if (!operacija.equals("") && brisi) {
-            ulaz.set("1");
+            ulaz.set(((Button)actionEvent.getSource()).getText());
             brisi = !brisi;
         } else if (!operacija.equals("")) {
-            ulaz.set(ulaz.get() + "1");
+            ulaz.set(ulaz.get() + ((Button)actionEvent.getSource()).getText());
         } else {
-            ulaz.set(ulaz.get() + "1");
-        }
-    }
-
-    public void dva(ActionEvent actionEvent) {
-        if (ulaz.get().equals("0") && operacija.equals("")) {
-            ulaz.set("2");
-        } else if (!operacija.equals("") && brisi) {
-            ulaz.set("2");
-            brisi = !brisi;
-        } else if (!operacija.equals("")) {
-            ulaz.set(ulaz.get() + "2");
-        } else {
-            ulaz.set(ulaz.get() + "2");
-        }
-    }
-
-    public void tri(ActionEvent actionEvent) {
-        if (ulaz.get().equals("0") && operacija.equals("")) {
-            ulaz.set("3");
-        } else if (!operacija.equals("") && brisi) {
-            ulaz.set("3");
-            brisi = !brisi;
-        } else if (!operacija.equals("")) {
-            ulaz.set(ulaz.get() + "3");
-        } else {
-            ulaz.set(ulaz.get() + "3");
-        }
-    }
-
-    public void cetiri(ActionEvent actionEvent) {
-        if (ulaz.get().equals("0") && operacija.equals("")) {
-            ulaz.set("4");
-        } else if (!operacija.equals("") && brisi) {
-            ulaz.set("4");
-            brisi = !brisi;
-        } else if (!operacija.equals("")) {
-            ulaz.set(ulaz.get() + "4");
-        } else {
-            ulaz.set(ulaz.get() + "4");
-        }
-    }
-
-    public void pet(ActionEvent actionEvent) {
-        if (ulaz.get().equals("0") && operacija.equals("")) {
-            ulaz.set("5");
-        } else if (!operacija.equals("") && brisi) {
-            ulaz.set("5");
-            brisi = !brisi;
-        } else if (!operacija.equals("")) {
-            ulaz.set(ulaz.get() + "5");
-        } else {
-            ulaz.set(ulaz.get() + "5");
-        }
-    }
-
-    public void sest(ActionEvent actionEvent) {
-        if (ulaz.get().equals("0") && operacija.equals("")) {
-            ulaz.set("6");
-        } else if (!operacija.equals("") && brisi) {
-            ulaz.set("6");
-            brisi = !brisi;
-        } else if (!operacija.equals("")) {
-            ulaz.set(ulaz.get() + "6");
-        } else {
-            ulaz.set(ulaz.get() + "6");
-        }
-    }
-
-    public void sedam(ActionEvent actionEvent) {
-        if (ulaz.get().equals("0") && operacija.equals("")) {
-            ulaz.set("7");
-        } else if (!operacija.equals("") && brisi) {
-            ulaz.set("7");
-            brisi = !brisi;
-        } else if (!operacija.equals("")) {
-            ulaz.set(ulaz.get() + "7");
-        } else {
-            ulaz.set(ulaz.get() + "7");
-        }
-    }
-
-    public void osam(ActionEvent actionEvent) {
-        if (ulaz.get().equals("0") && operacija.equals("")) {
-            ulaz.set("8");
-        } else if (!operacija.equals("") && brisi) {
-            ulaz.set("8");
-            brisi = !brisi;
-        } else if (!operacija.equals("")) {
-            ulaz.set(ulaz.get() + "8");
-        } else {
-            ulaz.set(ulaz.get() + "8");
-        }
-    }
-
-    public void devet(ActionEvent actionEvent) {
-        if (ulaz.get().equals("0") && operacija.equals("")) {
-            ulaz.set("9");
-        } else if (!operacija.equals("") && brisi) {
-            ulaz.set("9");
-            brisi = !brisi;
-        } else if (!operacija.equals("")) {
-            ulaz.set(ulaz.get() + "9");
-        } else {
-            ulaz.set(ulaz.get() + "9");
-        }
-    }
-
-    public void nula(ActionEvent actionEvent) {
-        if (ulaz.get().equals("0") && operacija.equals("")) {
-            ulaz.set("0");
-        } else if (!operacija.equals("") && brisi) {
-            ulaz.set("0");
-            brisi = !brisi;
-        } else if (!operacija.equals("")) {
-            ulaz.set(ulaz.get() + "0");
-        } else {
-            ulaz.set(ulaz.get() + "0");
+            ulaz.set(ulaz.get() + ((Button)actionEvent.getSource()).getText());
         }
     }
 
