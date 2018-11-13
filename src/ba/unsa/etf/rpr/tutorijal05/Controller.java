062fe70bd2e41381b0069b1077865b77ff50b592
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-13 17:48:18 +0100

    Dodane opcije za ostale operacije

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -4,13 +4,16 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+
+import javax.swing.*;
 
 public class Controller {
 
     private SimpleStringProperty ulaz;
     private double zbroj = 0;
     private double razlika = 0;
-    private double umnzoak = 1;
+    private double umnozak = 1;
     private double kolicnik = 1;
     private String operacija = "";
     private boolean brisi = false;
@@ -41,7 +44,10 @@ public class Controller {
     }
 
     public void zarez(ActionEvent actionEvent) {
-        ulaz.set(ulaz.get() + ".");
+        if(ulaz.get().contains("."));
+        else {
+            ulaz.set(ulaz.get() + ".");
+        }
     }
 
     public void plus(ActionEvent actionEvent) {
@@ -50,10 +56,56 @@ public class Controller {
         brisi = true;
     }
 
+    public void minus(ActionEvent actionEvent) {
+        razlika += Double.parseDouble(ulaz.get());
+        operacija = "-";
+        brisi = true;
+    }
+
+    public void puta(ActionEvent actionEvent) {
+        umnozak *= Double.parseDouble(ulaz.get());
+        operacija = "*";
+        brisi = true;
+    }
+
+    public void dijeljenje(ActionEvent actionEvent) {
+        kolicnik = Double.parseDouble(ulaz.get()) / kolicnik;
+        operacija = "/";
+        brisi = true;
+    }
+
+    public void posto(ActionEvent actionEvent) {
+        operacija="%";
+        brisi = true;
+    }
+
     public void jednako(ActionEvent actionEvent) {
         if (operacija.equals("+")) {
             ulaz.set(String.valueOf((Double.parseDouble(ulaz.get()) + zbroj)));
             operacija = "";
+            zbroj = 0;
         }
+        else if (operacija.equals("-")) {
+            if(razlika - Double.parseDouble(ulaz.get()) - (int) (razlika - Double.parseDouble(ulaz.get())) == 0)
+            ulaz.set(String.valueOf((razlika - Integer.parseInt(ulaz.get()))));
+            else ulaz.set(String.valueOf((razlika - Double.parseDouble(ulaz.get()))));
+            operacija = "";
+            razlika = 0;
+        }
+        else if (operacija.equals("*")) {
+            ulaz.set(String.valueOf((Double.parseDouble(ulaz.get()) * umnozak)));
+            operacija = "";
+            umnozak = 1;
+        }
+        else if (operacija.equals("/")) {
+            ulaz.set(String.valueOf((kolicnik / Double.parseDouble(ulaz.get()))));
+            operacija = "";
+            kolicnik = 1;
+        }
+        else if (operacija.equals("%")) {
+            ulaz.set(String.valueOf(Double.parseDouble(ulaz.get()) * 0.01));
+            operacija = "";
+        }
+
     }
 }
